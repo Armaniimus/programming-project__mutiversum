58a266244a75370df2966b5191b13a022205b63f
Armand_laptop <517226@edu.rocmn.nl>
2018-05-31 13:57:11 +0200

    added functions to get the required table data

--- programmeer project/model/ContentLogic.php
+++ programmeer project/model/ContentLogic.php
@@ -11,30 +11,32 @@ class ContentLogic {
     public $dataNullArray;
 
     public function __construct($dbName, $username, $pass, $serverAdress, $dbType) {
-        $this->PhpUtilities     = new PhpUtilities      ();
-        $this->DataHandler      = new DataHandler       ($dbName, $username, $pass, $serverAdress, $dbType);
+        $this->PhpUtilities     =   new PhpUtilities      ();
+        $this->DataHandler      =   new DataHandler       ($dbName, $username, $pass, $serverAdress, $dbType);
 
         $columnNames            =   $this->DataHandler->GetColumnNames("3dbril");
         $dataTypes              =   $this->DataHandler->GetTableTypes("3dbril");
         $dataNullArray          =   $this->DataHandler->GetTableNullValues("3dbril");
 
-        $this->DataValidator    = new DataValidator     ($columnNames, $dataTypes, $dataNullArray);
+        $this->DataValidator    =   new DataValidator     ($columnNames, $dataTypes, $dataNullArray);
     }
 
     public function __destruct() {
         $this->DataHandler = NULL;
     }
 
-    public function Get404Data($page) {
-        // set sql query
-        $sql = "SELECT title, content, keywords, description, author_users_id as author FROM content WHERE name = '$page'";
-        return $this->DataHandler->ReadSingleData($sql);
+    public function GetSpecificData() {
+        $sql = "SELECT naam, merk, prijs, afbeelding FROM 3dbril";
+        $returnArray = $this->ContentLogic->GetDefaultData();
+
+        return $returnArray;
     }
 
-    public function GetDefaultData($page) {
-        // set sql query
-        $sql = "SELECT title, content, keywords, description, author_users_id as author FROM content WHERE name = '$page'";
-        return $this->DataHandler->ReadSingleData($sql);
+    public function GetOverViewData() {
+        $sql = "SELECT naam, merk, prijs, afbeelding FROM 3dbril";
+        $returnArray = $this->ContentLogic->GetDefaultData();
+
+        return $returnArray;
     }
 }
 ?>
