1fc3b6ed0ad2f96be145897b3dfcc355c8d2ab36
youssra-4a6b <509717@edu.rocmn.nl>
2018-06-20 21:42:57 +0200

    files structures fixed

--- Project/partials/header.php
+++ Project/partials/header.php
@@ -17,7 +17,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script> 
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #bdc3c7">
-    <a class="navbar-brand text-dark" href="index.php"><img src="vr.png" height="50px"/>Multiversum</a>
+    <a class="navbar-brand text-dark" href="index.php"><img src="assets/images/vr.png" height="50px"/>Multiversum</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation"> 
     <span class="navbar-toggler-icon"></span>
     </button>
