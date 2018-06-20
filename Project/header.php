c50bdd21416b71c3a608fd89335190c1c9024a3f
youssra-4a6b <509717@edu.rocmn.nl>
2018-06-20 21:43:50 +0200

    links fixed

---
+++ Project/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" > 
+
+    <!-- font awsome css -->
+    <link rel="stylesheet" href="css/fontAwesome-all.min.css" >
+
+  </head>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script> 
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #bdc3c7">
+    <a class="navbar-brand text-dark" href="index.php"><img src="assets/images/vr.png" height="50px"/>Multiversum</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation"> 
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="producten.php">Producten</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="contact.php">Contact</a>
+      </li>
+    </ul>
+    
+    <button class="btn btn-sm btn-outline-secondary mr-3" type="button">
+    <i class="fas fa-shopping-cart fa-fw"></i> Winkelwagen
+  </button>
+
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Zoeken">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Zoeken</button>
+    </form>
+  </div>
+</nav>
+</html>
