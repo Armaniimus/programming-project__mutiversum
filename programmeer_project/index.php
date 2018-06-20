d9d52a97feb2d292e5dc2c18116dfbad38b46912
Armand_laptop <517226@edu.rocmn.nl>
2018-06-20 11:52:20 +0200

    fixed the timeout between admin actions

--- programmeer_project/index.php
+++ programmeer_project/index.php
@@ -7,20 +7,22 @@ $time = time();
 
 // start session
 ini_set('session.gc_maxlifetime', $expireTime); // sets time until marked as garbage on the server
-ini_set('session.cookie_lifetime', $maxExpireTime); // sets time until the cookie is trown away in the browser
+ini_set('session.cookie_lifetime', $maxExpireTime); // sets time until the cookie is thrown away in the browser
 session_start();
 
 // check if expiration time is smaller or equal then the difference between stored time vs current time
 // if yes store current time
 // if no destroy and restart session
 if (isset($_SESSION['timeout'])) {
-    if ($t - $_SESSION['timeout'] <= $expireTime) {
-        $_SESSION['timeout'] = time();
+    if ($time - $_SESSION['timeout'] <= $expireTime) {
+        $_SESSION['timeout'] = $time;
     } else {
         session_unset();
         session_destroy();
         session_start();
     }
+} else {
+    $_SESSION['timeout'] = $time;
 }
 
 // run normal program
