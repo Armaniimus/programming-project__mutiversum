436ff0f698cc270acbed2233242beff7b37d82c2
youssra-4a6b <509717@edu.rocmn.nl>
2018-06-20 22:03:58 +0200

    contact layout

--- Project/contact.php
+++ Project/contact.php
@@ -3,3 +3,12 @@
 include "partials/header.php";
 
 ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-8">
+			<div id="map" style="width:400px;height:400px">
+		</div>
+  		<div class="col-4">col-4</div>
+	</div>	
+</div>
