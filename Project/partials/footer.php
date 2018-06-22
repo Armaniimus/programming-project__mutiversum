dd8900dc1cd00450b955474b984eff09abec7bf1
youssra-4a6b <509717@edu.rocmn.nl>
2018-06-22 20:55:18 +0200

    fixed text footer

--- Project/partials/footer.php
+++ Project/partials/footer.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></head>
 <body>
 
-	<div class="card-footer text-muted" style="background-color: #bdc3c7; width: 100%; position: fixed; bottom: 0; text-align: left;">
+	<div class="card-footer text-muted" style="background-color: #bdc3c7; width: 100%; position: fixed; bottom: 0; text-align: center;">
 		Footer
 	</div>
 
