f31fc09b9a50336f79f6a369e6e0f506703187fa
youssra-4a6b <509717@edu.rocmn.nl>
2018-06-22 20:44:48 +0200

    updated footer

--- Project/partials/footer.php
+++ Project/partials/footer.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></head>
 <body>
 
-	<div class="footer">
-		<div>footer</div>
+	<div class="card-footer text-muted" style="background-color: #bdc3c7; width: 100%; position: fixed; bottom: 0; text-align: left;">
+		Footer
 	</div>
 
 </body>
