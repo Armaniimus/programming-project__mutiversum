137912103f7a061ce688c9b0cb85c614638d261e
youssra-4a6b <509717@edu.rocmn.nl>
2018-06-22 21:28:12 +0200

    remove onbelangrijke link

--- Project/header.php
+++ Project/header.php
@@ -14,8 +14,6 @@
     <!-- Google font Quicksand -->
     <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
 
-    <link rel="stylesheet" type="text/css" href="">
-
   </head>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
