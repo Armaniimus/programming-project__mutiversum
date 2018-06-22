b775d3b7d469779788bb27580307f010db392a06
Armand_laptop <517226@edu.rocmn.nl>
2018-06-23 00:19:09 +0200

    fixed layout bug in specific productview

--- programmeer_project/view/specific.php
+++ programmeer_project/view/specific.php
@@ -3,7 +3,7 @@
     <?php include "partials/meta.php"; ?>
 <body>
     <?php include "partials/header.php"; ?>
-    <main>
+    <main class="row">
         <div class="float-l col-xs-0    col-s-1     col-l-3"><br></div>
 
         <div class="float-l col-xs-12   col-s-10    col-l-6">
