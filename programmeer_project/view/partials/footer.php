9d5e0a62cf691516e5fb74b90d4e68c60b399954
youssra-4a6b <509717@edu.rocmn.nl>
2018-06-23 14:37:58 +0200

    updatet footer

--- programmeer_project/view/partials/footer.php
+++ programmeer_project/view/partials/footer.php
@@ -1,4 +1,14 @@
-<br>
-<footer class="card-footer text-muted" style="background-color: #343a40; width: 100%; position: relative;  text-align: center;">
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></head>
+<body>
+
+	<div class="card-footer text-muted mt-5" style="background-color: #bdc3c7; width: 100%; bottom: 0; text-align: center;">
 		Copyright © 2018 Multiversum
-</footer>
+	</div>
+
+</body>
+</html>
