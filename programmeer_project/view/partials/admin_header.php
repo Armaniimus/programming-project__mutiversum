24b5b3d905e5c516a32800a8c32fc498f959519a
Armand_laptop <517226@edu.rocmn.nl>
2018-06-23 19:29:03 +0200

    made admin header and normal header more consistent

--- programmeer_project/view/partials/admin_header.php
+++ programmeer_project/view/partials/admin_header.php
@@ -3,11 +3,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #bdc3c7">
 
         <!-- logo -->
-        <a class="navbar-brand" href="index.php?view=admin">
-            <img src="view/assets/images/vr.svg" height="50px"/>Multiversum
+        <a class="navbar-brand text-dark" href="index.php">
+            <img src="view/assets/images/vr.png" height="50px"/>Multiversum
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -16,10 +16,10 @@
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" href="index.php?view=admin">Home</a>
+                    <a class="nav-link text-dark" href="index.php?view=admin">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?view=admin_products">Producten</a>
+                    <a class="nav-link text-dark" href="index.php?view=admin_products">Producten</a>
                 </li>
             </ul>
 
--- programmeer_project/view/partials/header.php
+++ programmeer_project/view/partials/header.php
@@ -1,34 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  </head>
-
+<header>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
+
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #bdc3c7">
-    <a class="navbar-brand text-dark" href="index.php"><img src="view/assets/images/vr.png" height="50px"/>Multiversum</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item">
-        <a class="nav-link text-dark" href="index.php">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-dark" href="index.php?view=products">Producten</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-dark" href="index.php?view=contact">Contact</a>
-      </li>
-    </ul>
 
-    <a href="index.php?view=winkelwagen" class="btn btn-sm btn-outline-secondary mr-3">Winkelwagen</a>
+        <a class="navbar-brand text-dark" href="index.php">
+            <img src="view/assets/images/vr.png" height="50px"/>Multiversum
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="index.php?view=products">Producten</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="index.php?view=contact">Contact</a>
+                </li>
+            </ul>
+
+            <a href="index.php?view=winkelwagen" class="btn btn-sm btn-outline-secondary mr-3">Winkelwagen</a>
 
             <?php
                 if (!isset($previousSearch)) {
@@ -39,6 +37,6 @@
                   <input class="form-control mr-sm-2" name="search" placeholder="Zoeken" value="<?php echo $previousSearch ?>">
                   <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Zoeken</button>
             </form>
-  </div>
-</nav>
-</html>
+        </div>
+    </nav>
+</header>
