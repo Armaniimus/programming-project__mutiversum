fc5999110b5b9d201d89f7faace68c244482bd67
youssra-4a6b <509717@edu.rocmn.nl>
2018-06-23 14:30:22 +0200

    updatet new header

--- programmeer_project/view/partials/header.php
+++ programmeer_project/view/partials/header.php
@@ -1,27 +1,47 @@
-<header>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" > 
+
+    <!-- font awsome css -->
+    <link rel="stylesheet" href="css/fontAwesome-all.min.css" >
+
+    <!-- Google font Quicksand -->
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+
+    <!-- style.css -->
+    <link rel="stylesheet" href="/assets/css/style.css" >
+
+  </head>
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.php"><img src="view/assets/images/vr.png" height="50px"/>Multiversum</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script> 
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #bdc3c7">
+    <a class="navbar-brand text-dark" href="index.php"><img src="view/assets/images/vr.png" height="50px"/>Multiversum</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation"> 
+    <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?view=products">Producten</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?view=contact">Contact</a>
-                </li>
-            </ul>
-
-            <a href="index.php?view=winkelwagen" class="btn btn-sm btn-outline-secondary mr-3">Winkelwagen</a>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="producten.php">Producten</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="contact.php">Contact</a>
+      </li>
+    </ul>
+    
+    <a href="index.php?view=winkelwagen" class="btn btn-sm btn-outline-secondary mr-3">Winkelwagen</a>
 
             <?php
                 if (!isset($previousSearch)) {
@@ -32,6 +52,6 @@
                   <input class="form-control mr-sm-2" name="search" placeholder="Zoeken" value="<?php echo $previousSearch ?>">
                   <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Zoeken</button>
             </form>
-        </div>
-    </nav>
-</header>
+  </div>
+</nav>
+</html>
