b54e90aa8e1875aa4e428923364cf4a3f970f707
youssra-4a6b <509717@edu.rocmn.nl>
2018-06-25 01:09:34 +0200

    changed form

--- programmeer_project/view/form.php
+++ programmeer_project/view/form.php
@@ -1,32 +1,30 @@
 <form>
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+      ID:
+      <input type="number" class="form-control" id="inputID">
     </div>
     <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+      Naam:
+      <input type="text" class="form-control" id="inputNaam" placeholder="Password">
     </div>
   </div>
   <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+    Model:
+    <input type="text" class="form-control" id="inputModel">
   </div>
   <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
     <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
       <input type="text" class="form-control" id="inputCity">
     </div>
     <div class="form-group col-md-4">
-      <label for="inputState">State</label>
+      3d/2d
       <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
+        <option selected>3d</option>
+        <option>2d</option>
       </select>
     </div>
     <div class="form-group col-md-2">
