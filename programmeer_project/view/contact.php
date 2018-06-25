19832cbade71abe3f9cbc7dc7d2531d0d96cc9e0
youssra-4a6b <509717@edu.rocmn.nl>
2018-06-26 01:31:15 +0200

    added google map but without API key

--- programmeer_project/view/contact.php
+++ programmeer_project/view/contact.php
@@ -6,22 +6,36 @@
 	<main>
         <div class="row">
             <div class="float-l col-xs-2"><br></div>
-            <div class="float-l col-xs-8 login--center">
+            <div class="float-l col-xs-8 login--center" style="float: right;">
                 <h3>Contact</h3>
-                <div class="contact--centerblok">
+                <div id="map" style="width:600px; height:400px;"></div>
+
+                <script>
+                    function myMap() {
+                    var mapOptions = {
+                        center: new google.maps.LatLng(51.5, -0.12),
+                        zoom: 10,
+                        mapTypeId: google.maps.MapTypeId.HYBRID
+                    }
+                    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+                    }
+                </script>
+
+                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
+
                     <h5>Retouren</h5>
                     <p>
-                        Wilt u Retourneren Mail dan aub naar hanneke@multiversum.nl<br><br>
+                        Wilt u retourneren? Mail dan aub naar hanneke@multiversum.nl<br><br>
                     </p>
 
                     <h5>Zakelijk verkeer</h5>
                     <p>
-                        Voor zakelijke verkeer kunt u terecht bij jackjones@multiversum.nl of 06-85549877
+                        Voor zakelijk verkeer kunt u terecht bij jackjones@multiversum.nl of 06-85549877
                     </p>
 
                     <h5>Vragen, klachten</h5>
                     <p>
-                        Mocht u vragen hebben of een klacht dan kunt u ons mailen op hanneke@multiversum.nl
+                        Mocht u vragen hebben of een klacht, dan kunt u ons mailen op hanneke@multiversum.nl
                         of u kunt bellen naar 06-85549876.
                     </p>
 
