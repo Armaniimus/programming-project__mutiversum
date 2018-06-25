04d5ea9b99136fde1eb74dbd6691b406061b59de
youssra-4a6b <509717@edu.rocmn.nl>
2018-06-26 01:33:49 +0200

    Openingstijden toegvoegd aan contact

--- programmeer_project/view/contact.php
+++ programmeer_project/view/contact.php
@@ -25,7 +25,16 @@
 
                     <h5>Retouren</h5>
                     <p>
-                        Wilt u retourneren? Mail dan aub naar hanneke@multiversum.nl<br><br>
+                        Wilt u retourneren? Mail dan aub naar hanneke@multiversum.nl<br>
+                    </p>
+                    <h5>Openingstijden</h5>
+                    <p>
+                        Maandag: 09:00-17:00 <br>
+                        Dinsdag: 09:00-17:00<br>
+                        Woensdag: 09:00-17:00<br>
+                        Donderdag: 09:00-17:00<br>
+                        Vrijdag:  09:00-17:00<br>
+                        Zaterdag/Zondag: GESLOTEN.
                     </p>
 
                     <h5>Zakelijk verkeer</h5>
