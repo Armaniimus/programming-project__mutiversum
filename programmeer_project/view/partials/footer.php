300f5b1804b242f3fd4582cf0a7aac00d2d14445
youssra-4a6b <509717@edu.rocmn.nl>
2018-06-25 17:26:39 +0200

    added facebook and instagram picture

--- programmeer_project/view/partials/footer.php
+++ programmeer_project/view/partials/footer.php
@@ -5,10 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></head>
 <body>
-
+<footer>
 	<div class="card-footer text-muted mt-5" style="background-color: #bdc3c7; width: 100%; position: relative; text-align: center;">
 		Copyright © 2018 Multiversum
 	</div>
-
+</footer>
 </body>
 </html>
